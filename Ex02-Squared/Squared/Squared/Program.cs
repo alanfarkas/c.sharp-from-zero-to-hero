90d74a2206add5798b60f979833627ea9cb0b40a
Alan Farkas <afarkas@alvarezandmarsal.com>
2016-04-26 15:09:56 -0500

    ex02

---
+++ Ex02-Squared/Squared/Squared/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, ans = 0;
+
+            Console.WriteLine("Enter a number to square:");
+            num = Convert.ToInt32(Console.ReadLine());
+            ans = num * num;
+            Console.WriteLine("\n{0} Squared is: {1}", num, ans);
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
