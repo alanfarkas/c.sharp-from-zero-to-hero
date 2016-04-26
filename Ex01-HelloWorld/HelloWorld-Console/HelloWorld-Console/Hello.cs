9ab72901602fc3bc061ab576776636cdb082d8d1
Alan Farkas <afarkas@alvarezandmarsal.com>
2016-04-26 14:10:12 -0500

    ex01

---
+++ Ex01-HelloWorld/HelloWorld-Console/HelloWorld-Console/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Console
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+
+#if DEBUG
+    Console.WriteLine("Press enter to close...");
+    Console.ReadLine();
+#endif
+
+        }
+    }
+}
